9d7ee530f32bdb683ac213ec74642a345bc11809
Phillip Kruger <phillip.kruger@gmail.com>
2018-05-29 10:53:09 +0200

    Started with User Services (Open Liberty)

--- user/src/main/java/com/github/phillipkruger/user/TokenService.java
+++ user/src/main/java/com/github/phillipkruger/user/TokenService.java
@@ -1,37 +1,34 @@
 package com.github.phillipkruger.user;
 
 import javax.enterprise.context.RequestScoped;
-import javax.inject.Inject;
-import javax.validation.constraints.NotNull;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import lombok.extern.java.Log;
 import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
-import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
-import org.eclipse.microprofile.rest.client.inject.RestClient;
-import java.util.logging.Level;
-import javax.ws.rs.NotFoundException;
-import javax.ws.rs.core.Response.Status;
-import org.eclipse.microprofile.openapi.annotations.headers.Header;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import static com.github.phillipkruger.user.SecurityRoles.ADMIN_ROLE;
+import static com.github.phillipkruger.user.SecurityRoles.USER_ROLE;
+import java.security.Principal;
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
+import javax.json.Json;
+import javax.json.JsonObjectBuilder;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.SecurityContext;
 
 /**
  * Token Service. JAX-RS
  * @author Phillip Kruger (phillip.kruger@phillip-kruger.com)
+ * @see https://docs.payara.fish/documentation/microprofile/jwt.html
+ * @see https://github.com/javaee-samples/microprofile1.2-samples/tree/master/jwt-auth
  */
+@DeclareRoles({USER_ROLE,ADMIN_ROLE})
 @Log
 @RequestScoped
 @Path("/token")
@@ -39,9 +36,40 @@ import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 @Tag(name = "Token service",description = "JWT Issuer")
 public class TokenService {
     
-    @POST @GET
-    //@Operation(description = "Getting all the events for a certain user")
-    //@APIResponse(responseCode = "202", description = "Accepted the event, queued to be stored")
+    @Context
+    private SecurityContext securityContext;
+    
+    @GET
+    @Operation(description = "Ping to test")
+    @APIResponse(responseCode = "200", description = "Received the ping, return a pong")
+    @RolesAllowed({ "admin", "user" })
+    public Response ping(){
+        
+        String authenticationScheme = securityContext.getAuthenticationScheme();
+        log.severe(">> authenticationScheme = " + authenticationScheme);
+        
+        boolean secure = securityContext.isSecure();
+        log.severe(">> secure = " + secure);
+        
+        boolean isAdmin = securityContext.isUserInRole(ADMIN_ROLE);
+        log.severe(">> isAdmin = " + isAdmin);
+        
+        boolean isUser = securityContext.isUserInRole(USER_ROLE);
+        log.severe(">> isUser = " + isUser);
+        
+        Principal userPrincipal = securityContext.getUserPrincipal();
+        String username = userPrincipal.getName();
+        log.severe(">> username = " + username);
+        
+        JsonObjectBuilder b = Json.createObjectBuilder();
+        b.add("ping", "pong");
+        
+        return Response.ok(b.build().toString()).build();
+    }
+    
+    @POST
+    @Operation(description = "Issue a JWT Token for the logged in user")
+    @APIResponse(responseCode = "200", description = "Received the ping, return a pong")
     public Response issueToken(){
         
         return Response.ok("TODO").build();
