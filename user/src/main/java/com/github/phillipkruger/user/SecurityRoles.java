a8f8d9fddad3b4bda60cbec8f5f5616cf63244c7
Phillip Kruger <phillip.kruger@gmail.com>
2018-06-07 10:54:06 +0200

    Added JWT token

--- user/src/main/java/com/github/phillipkruger/user/SecurityRoles.java
+++
@@ -1,6 +0,0 @@
-package com.github.phillipkruger.user;
-
-public final class SecurityRoles {
-    public static final String USER_ROLE = "user";
-    public static final String ADMIN_ROLE = "admin";
-}
